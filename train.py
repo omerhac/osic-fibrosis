66789ea604f71d59f7e5ce12f05cf95f5ecad924
omerhac <omerhac94@gmail.com>
2020-09-01 18:11:06 +0300

    Working theta model

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import models
 import tensorflow as tf
 import visualize
 import pandas as pd
+import numpy as np
 
 # CNN Constants
 CNN_EPOCHS = 20
@@ -11,7 +12,7 @@ CNN_STEPS_PER_EPOCH = 32994 // CNN_BATCH_SIZE
 CNN_IMAGE_SIZE = (224, 224)
 
 # Theta constants
-THETA_EPOCHS = 20
+THETA_EPOCHS = 1500
 THETA_BATCH_SIZE = 128
 THETA_STEPS_PER_EPOCH = 32994 // CNN_BATCH_SIZE
 
@@ -46,22 +47,27 @@ def train_cnn_model(save_path):
 def train_theta_model(save_path, cnn_model_path='models_weights/cnn_model/model_v2.ckpt'):
     """Train the theta predicting model. Save weights to models_weights/theta_model. Return history dict"""
     # get datasets
-    dataset = pd.read_csv('theta_data/pp_train.csv')#etl.create_nn_train(model_path=cnn_model_path)
+    dataset = pd.read_csv('theta_data/pp_train.csv').drop(["Unnamed: 0"], axis=1)#etl.create_nn_train(model_path=cnn_model_path)
     train_ids, val_ids = etl.get_train_val_split()
+
+    # cast dtypes
+    numeric_columns = dataset.select_dtypes(include=np.number).columns
+    dataset[numeric_columns] = dataset[numeric_columns].astype('float32')
+
     train_dataset = dataset.loc[dataset["Patient"].isin(train_ids)]  # get train rows
     val_dataset = dataset.loc[dataset["Patient"].isin(val_ids)]  # get validation rows
 
     # split target
-    train_y = train_dataset["Theta"]
-    val_x = val_dataset["Theta"]
-    train_x = train_dataset.drop(["Theta"], axis=1)
-    val_y = val_dataset.drop(["Theta"], axis=1)
+    train_y = train_dataset["Theta"].values
+    val_y = val_dataset["Theta"].values
+    train_x = train_dataset.drop(["Theta", "Patient"], axis=1).values  # drop target and patient id
+    val_x = val_dataset.drop(["Theta", "Patient"], axis=1).values  # drop target and patient id
 
     # get model
-    theta_model = models.get_theta_model(len(dataset.columns))
+    theta_model = models.get_theta_model(train_x.shape[1])  # input vector n_dim is n columns
 
     # add callbacks
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_mse', mode='auto',
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='auto',
                                                       restore_best_weights=True, patience=3, verbose=1)
 
     # train
@@ -77,4 +83,8 @@ def train_theta_model(save_path, cnn_model_path='models_weights/cnn_model/model_
 if __name__ == '__main__':
     # hist = train_cnn_model()
     hist = train_theta_model('models_weights/theta_model/theta_model_v1')
-    visualize.plot_training_curves(hist)
+    # visualize.plot_training_curves(hist)
+    pd.set_option('display.max_columns', None)
+    #dataset = pd.read_csv('theta_data/pp_train.csv').drop(["Unnamed: 0"], axis=1)
+    #print(dataset.head(5))
+
